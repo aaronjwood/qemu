997ad5d1aa870d8de3b7b66319bf8fafbd30be17
Aaron Wood <aaronjwood@gmail.com>
2016-09-15 16:33:49 -0400

    Make sure no forking processes can escape our emulation

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -7301,6 +7301,105 @@ static target_timer_t get_timer_id(abi_long arg)
     return timerid;
 }
 
+// Same size as defined in binfmts.h
+#define BINPRM_BUF_SIZE 128
+
+/*
+ * Script interpreter handling, kind of nasty.
+ * https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/tree/fs/binfmt_script.c
+ */
+static abi_long handle_interpreters(char buf[], char** i_arg, char** i_name, int* offset)
+{
+	char* cp;
+	if ((buf[0] == '#') && (buf[1] == '!')) {
+		buf[BINPRM_BUF_SIZE - 1] = '\0';
+		if ((cp = strchr(buf, '\n')) == NULL)
+			cp = buf+BINPRM_BUF_SIZE-1;
+		*cp = '\0';
+		while (cp > buf) {
+			cp--;
+			if ((*cp == ' ') || (*cp == '\t'))
+				*cp = '\0';
+			else
+				break;
+		}
+		for (cp = buf+2; (*cp == ' ') || (*cp == '\t'); cp++);
+		if (*cp == '\0')
+			return -ENOEXEC; /* No interpreter name found */
+		*i_name = cp;
+		*i_arg = NULL;
+		for ( ; *cp && (*cp != ' ') && (*cp != '\t'); cp++)
+			/* nothing */ ;
+		while ((*cp == ' ') || (*cp == '\t'))
+			*cp++ = '\0';
+		if (*cp)
+			*i_arg = cp;
+
+		if (*i_arg)
+			*offset = 5;
+		else
+			*offset = 4;
+	}
+	return 0;
+}
+
+/*
+ * This modifies how the the system call is constructed and executed.
+ * We want to make sure that we keep any forking processes jailed inside of the emulator.
+ */
+static abi_long arm_execve(char *filename, char *argv[], char *envp[])
+{
+    char *i_arg = NULL;
+    char *i_name = NULL;
+    char **new_argp;
+    int argc;
+    int fd;
+    int ret;
+    int i;
+	int offset = 3;
+    char buf[BINPRM_BUF_SIZE];
+
+    for (argc = 0; argv[argc] != NULL; argc++);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return -ENOENT;
+
+    ret = read(fd, buf, BINPRM_BUF_SIZE);
+    if (ret == -1) {
+        close(fd);
+        return -ENOENT;
+    }
+
+    close(fd);
+
+    if (handle_interpreters(buf, &i_arg, &i_name, &offset) != 0) {
+        return -ENOEXEC;
+    }
+
+    new_argp = alloca((argc + offset + 1) * sizeof(void *));
+
+    for (i = 0; i < argc; i++)
+        new_argp[i + offset] = argv[i];
+
+    new_argp[0] = strdup("/usr/bin/qemu-aarch64-static");
+    new_argp[1] = strdup("-0");
+    new_argp[offset] = filename;
+    new_argp[argc + offset] = NULL;
+
+    if (i_name) {
+        new_argp[2] = i_name;
+        new_argp[3] = i_name;
+
+        if (i_arg)
+            new_argp[4] = i_arg;
+    } else {
+        new_argp[2] = argv[0];
+    }
+
+    return get_errno(safe_execve("/usr/bin/qemu-aarch64-static", new_argp, envp));
+}
+
 /* do_syscall() should always have a single exit point at the end so
    that actions, such as logging of syscall results, can be performed.
    All errnos that do_syscall() returns must be -TARGET_<errcode>. */
@@ -7590,7 +7689,7 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
              * before the execve completes and makes it the other
              * program's problem.
              */
-            ret = get_errno(safe_execve(p, argp, envp));
+            ret = get_errno(arm_execve(p, argp, envp));
             unlock_user(p, arg1, 0);
 
             goto execve_end;
